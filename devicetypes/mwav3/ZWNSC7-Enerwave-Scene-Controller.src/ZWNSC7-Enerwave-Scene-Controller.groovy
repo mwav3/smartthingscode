708edabac30e37326219d9befecacf3ac1a9f611
Tim Grimley <65051965+mwav3@users.noreply.github.com>
2021-01-26 20:58:14 -0500

    Update and rename devicetypes/mwav3/ZWNSC7-Enerwave-7-Button-Scene-Controller.src/ZWNSC7-Enerwave-7-Button-Scene-Controller.groovy to devicetypes/mwav3/ZWNSC7-Enerwave-Scene-Controller.src/ZWNSC7-Enerwave-Scene-Controller.groovy
    name update

--- devicetypes/mwav3/ZWNSC7-Enerwave-Scene-Controller.src/ZWNSC7-Enerwave-Scene-Controller.groovy
+++ devicetypes/mwav3/ZWNSC7-Enerwave-Scene-Controller.src/ZWNSC7-Enerwave-Scene-Controller.groovy
@@ -24,7 +24,7 @@ import groovy.json.JsonOutput
 
  metadata {
   // Automatically generated. Make future change here.
-  definition (name: "ZWNSC7 Enerwave 7 Button Scene Controller", namespace: "mwav3", author: "Tim Grimley") {
+  definition (name: "ZWNSC7 Enerwave Scene Controller", namespace: "mwav3", author: "Tim Grimley") {
         capability "Actuator"
         capability "Button"
         capability "Configuration"
